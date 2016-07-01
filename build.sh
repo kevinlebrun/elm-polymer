2ae59a298e1d84facd7117aa16642979d4c4f821
Kevin Le Brun <lebrun.k@gmail.com>
2016-07-01 11:28:55 +0200

    Working Polymer components with Elm Html.Keyed.node

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 elm make counter-elm-with-paper-components/Counter.elm --output counter-elm-with-paper-components/card.js
+elm make counter-elm-with-paper-components-keyed-nodes/Counter.elm --output counter-elm-with-paper-components-keyed-nodes/card.js
 elm make counter-elm-inside-polymer/Counter.elm --output counter-elm-inside-polymer/counter.js
 elm make counter-everything-on-top/Counter.elm --output counter-everything-on-top/counter.js
 elm make counter-polymer-inside-elm/Main.elm --output counter-polymer-inside-elm/main.js
---
+++ counter-elm-with-paper-components-keyed-nodes/Counter.elm
@@ -0,0 +1,65 @@
+module Card exposing (..)
+
+import Html exposing (..)
+import Html.App exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Keyed
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 3, Cmd.none )
+
+
+paperCard =
+    node "paper-card"
+
+
+paperButton =
+    node "paper-button"
+
+
+view : Model -> Html Msg
+view model =
+    Html.Keyed.node "div"
+        []
+        [ ( toString model
+          , paperCard [ attribute "heading" ("Counter: " ++ (toString model)) ]
+                [ div [ class "card-content" ] [ text (toString model) ]
+                , div [ class "card-actions" ]
+                    [ paperButton [ onClick Decrement ] [ text "Decrement" ]
+                    , paperButton [ onClick Increment ] [ text "Increment" ]
+                    ]
+                ]
+          )
+        ]
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none )
+
+        Decrement ->
+            ( model - 1, Cmd.none )
+
+
+main : Program Never
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
